e5d94dc3bd0fce250028222cf611874239ff8440
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 21:03:10 +0900

    Q1.5

---
+++ Netsoft.OpenDataStructures.Chapter01/Question05.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace Netsoft.OpenDataStructures.Chapter01
+{
+    public class Bag
+    {
+        private readonly SortedSet<BagKey<int>> _set;
+        public Bag()
+        {
+            _set = new SortedSet<BagKey<int>>();
+        }
+
+        public void Add(int item)
+        {
+            var key = new BagKey<int>(item, 0);
+            var actual = new BagKey<int>(item, 0);
+            if (_set.Contains(new BagKey<int>(item, 0)))
+            {
+                _set.TryGetValue(key, out actual);
+                _set.Remove(key);
+            }
+            _set.Add(new BagKey<int>(item, actual.Value + 1));
+        }
+
+        public bool Remove(int item)
+        {
+            var key = new BagKey<int>(item, 0);
+            if (_set.Contains(new BagKey<int>(item, 0)))
+            {
+                _set.TryGetValue(key, out var actual);
+                _set.Remove(key);
+                _set.Add(new BagKey<int>(item, actual.Value > 0 ? actual.Value - 1 : 0));
+                return true;
+            }
+            return false;
+        }
+
+        public bool Find(int item)
+        {
+            return FindAll(item) > 0;
+        }
+
+        public int FindAll(int item)
+        {
+            var key = new BagKey<int>(item, 0);
+            if (_set.Contains(new BagKey<int>(item, 0)))
+            {
+                _set.TryGetValue(key, out var actual);
+                return actual.Value;
+            }
+
+            return 0;
+        }
+    }
+
+    class BagKey<V> : IComparable
+    {
+        private readonly int _key;
+
+        public BagKey(int key, V value)
+        {
+            _key = key;
+            Value = value;
+        }
+
+        public V Value { get; }
+        public int CompareTo(object obj)
+        {
+            if (obj is BagKey<V> key)
+            {
+                return _key.CompareTo(key._key);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Tests.Netsoft.OpenDataStructures/Chapter01/TestQuestion05.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+using Netsoft.OpenDataStructures.Chapter01;
+using System.Collections.Generic;
+
+namespace Tests.Netsoft.OpenDataStructures.Chapter01
+{
+    public class TestQuestion05
+    {
+        [Fact]
+        public void TestInit()
+        {
+            var bag = new Bag();
+
+            Assert.False(bag.Find(1));
+            Assert.False(bag.Remove(1));
+        }
+
+        [Fact]
+        public void TestAdd()
+        {
+            var bag = new Bag();
+
+            bag.Add(1);
+            bool got = bag.Find(1);
+            Assert.True(got);
+        }
+
+        [Fact]
+        public void TestAddMultiple()
+        {
+            var bag = new Bag();
+
+            bag.Add(1);
+            bag.Add(1);
+            Assert.True(bag.Find(1));
+            Assert.Equal(2, bag.FindAll(1));
+        }
+
+        [Fact]
+        public void TestRemove()
+        {
+            var bag = new Bag();
+
+            bag.Add(1);
+            bag.Remove(1);
+
+            Assert.False(bag.Find(1));
+            Assert.Equal(0, bag.FindAll(1));
+        }
+
+        [Fact]
+        public void TestRemoveMultiple()
+        {
+            var bag = new Bag();
+
+            bag.Add(1);
+            bag.Add(1);
+            bag.Add(2);
+            bag.Remove(1);
+
+            Assert.Equal(1, bag.FindAll(1));
+            Assert.Equal(1, bag.FindAll(2));
+        }
+    }
+}
