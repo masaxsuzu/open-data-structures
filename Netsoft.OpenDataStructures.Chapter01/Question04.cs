56677f8d39877e00470a1b8bd345b52217e2a8f9
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 20:10:45 +0900

    Q1.4

---
+++ Netsoft.OpenDataStructures.Chapter01/Question04.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace Netsoft.OpenDataStructures.Chapter01
+{
+    public static class Question04
+    {
+        public static IEnumerable<T> Answer<T>(Stack<T> @input)
+        {
+            var stack = new MyStack<T>();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            return stack;
+        }
+    }
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private Queue<T> _q1;
+        private Queue<T> _q2;
+
+        public MyStack()
+        {
+            _q1 = new Queue<T>();
+            _q2 = new Queue<T>();
+
+            Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public T Pop()
+        {
+            if (Count <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            
+
+            for (int i = 0; i < Count -1 ; i++)
+            {
+                _q2.Enqueue(_q1.Dequeue());
+            }
+
+            var item = _q1.Dequeue();
+
+            // Swap queues
+            var _ = _q2;
+            _q2 = _q1;
+            _q1 = _;
+
+            Count--;
+
+            return item;
+        }
+
+        public void Push(T item)
+        {
+            _q1.Enqueue(item);
+            Count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while(Count > 0)
+            {
+                yield return Pop();
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Tests.Netsoft.OpenDataStructures/Chapter01/TestQuestion04.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+using Netsoft.OpenDataStructures.Chapter01;
+using System.Collections.Generic;
+
+namespace Tests.Netsoft.OpenDataStructures.Chapter01
+{
+    public class TestQuestion04
+    {
+        [Fact]
+        public void TestAnswer()
+        {
+            var stack = new Stack<int>(new int[] { 1, 2, 3 });
+            var actual = Question04.Answer(stack);
+            Assert.Equal(new int[] {1, 2, 3}, actual.ToArray());
+        }
+    }
+}
