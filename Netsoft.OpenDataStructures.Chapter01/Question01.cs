f0890744a97ff52def3b42435b90c4e048e13d09
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 16:50:57 +0900

    Q1.1.2

--- Netsoft.OpenDataStructures.Chapter01/Question01.cs
+++ Netsoft.OpenDataStructures.Chapter01/Question01.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Netsoft.OpenDataStructures.Chapter01
 {
-    public class Question01
+    public static class Question01
     {
         public static void Answer01(StreamReader stdin, StreamWriter stdout)
         {
@@ -17,6 +18,27 @@ namespace Netsoft.OpenDataStructures.Chapter01
             {
                 stdout.WriteLine(lines.Pop());
             }
+        }
+
+        public static void Answer02(StreamReader stdin, StreamWriter stdout)
+        {
+            while (!stdin.EndOfStream)
+            {
+                foreach (string line in stdin.ReadLines(50).Reverse())
+                {
+                    stdout.WriteLine(line);
+                }
+            }
+        }
+
+        private static string[] ReadLines(this StreamReader stdin, int by)
+        {
+            var lines = new List<string>();
+            while (lines.Count < by || !stdin.EndOfStream)
+            {
+                lines.Add(stdin.ReadLine());
+            }
+            return lines.ToArray();
         }
     }
 }
