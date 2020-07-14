57b55d36f245d9ee4d9fb13c4f73d85f751647a2
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 21:40:38 +0900

    Q1.6.1

---
+++ Netsoft.OpenDataStructures.Chapter01/Question06.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace Netsoft.OpenDataStructures.Chapter01
+{
+    public class MyList
+    {
+        private int[] _collection;
+        public MyList()
+        {
+            _collection = new int[0] { };
+        }
+        
+        public int Size()
+        {
+           return  _collection.Length;
+        }
+
+        public int Get(int i)
+        {
+            return _collection[i];
+        }
+
+        public void Set(int i, int v)
+        {
+            _collection[i] = v;
+        }
+
+        public void Add(int i, int v)
+        {
+            int n = Size() + 1;
+            Array.Resize(ref _collection, n);
+
+            for (int index = n - 1; index > i ; index--)
+            {
+                _collection[index] = _collection[index - 1];
+            }
+
+            _collection[i] = v;
+        }
+
+        public void Remove(int i)
+        {
+
+            for (int index = i; index < Size() - 1; index++)
+            {
+                _collection[index] = _collection[index + 1];
+            }
+
+            Array.Resize(ref _collection, Size() -1);
+        }
+    }
+}
