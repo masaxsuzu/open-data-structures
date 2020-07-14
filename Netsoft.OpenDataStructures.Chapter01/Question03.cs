1345d5ae8dba5ecd799283db94ec2822e1c46217
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 18:47:57 +0900

    Q1.3

---
+++ Netsoft.OpenDataStructures.Chapter01/Question03.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Netsoft.OpenDataStructures.Chapter01
+{
+    public static class Question03
+    {
+        public static bool Answer(string[] input)
+        {
+            var stack = new Stack<string>();
+            int n = 0;
+            while(n < input.Length)
+            {
+                if(!_try(stack, input, ref n))
+                {
+                    return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+        private static bool _try(Stack<string> stack, string[] input, ref int n)
+        {
+            string v = input[n];
+            n++;
+            switch (v)
+            {
+                case "{":
+                    stack.Push(v);
+                    return isClosed("}", stack, input, ref n);
+                case "(":
+                    stack.Push(v);
+                    return isClosed(")", stack, input, ref n);
+                case "[":
+                    stack.Push(v);
+                    return isClosed("]", stack, input, ref n);
+                default:
+                    return false;
+            }
+        }
+        private static bool isClosed(string closing, Stack<string> stack, string[] input, ref int n)
+        {
+            do
+            {
+                string v = input[n];
+                if(v == closing)
+                {
+                    n++;
+                    stack.Pop();
+                    return true;
+                }
+                if(!_try(stack, input, ref n))
+                {
+                    return false;
+                }
+            } while (n < input.Length);
+            return false;
+        }
+    }
+}
---
+++ Tests.Netsoft.OpenDataStructures/Chapter01/TestQuestion03.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+using Netsoft.OpenDataStructures.Chapter01;
+
+namespace Tests.Netsoft.OpenDataStructures.Chapter01
+{
+    public class TestQuestion03
+    {
+        [Theory]
+        [InlineData("(,)", true)]
+        [InlineData("{,}", true)]
+        [InlineData("[,]", true)]
+        [InlineData("[,(,),]", true)]
+        [InlineData("[,{,},]", true)]
+        [InlineData("[,[,],]", true)]
+        [InlineData("[,[,],)", false)]
+        [InlineData("[,[,),]", false)]
+        [InlineData("[,(,],]", false)]
+        [InlineData("{,{,(,),[,],},}", true)]
+        [InlineData("{,{,(,),],}", false)]
+        public void TestAnswer(string @input, bool expected)
+        {
+            bool actual = Question03.Answer(@input.Split(','));
+            Assert.Equal(expected, actual);
+        }
+    }
+}
