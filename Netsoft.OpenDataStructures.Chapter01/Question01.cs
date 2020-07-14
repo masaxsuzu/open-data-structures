7031e0a6579ef2c72b03a23640680696098bed1f
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 17:22:17 +0900

    Q1.1.4

--- Netsoft.OpenDataStructures.Chapter01/Question01.cs
+++ Netsoft.OpenDataStructures.Chapter01/Question01.cs
@@ -10,11 +10,11 @@ namespace Netsoft.OpenDataStructures.Chapter01
         public static void Answer01(StreamReader stdin, StreamWriter stdout)
         {
             var lines = new Stack<string>();
-            while(!stdin.EndOfStream)
+            while (!stdin.EndOfStream)
             {
                 lines.Push(stdin.ReadLine());
             }
-            while(lines.Count != 0)
+            while (lines.Count != 0)
             {
                 stdout.WriteLine(lines.Pop());
             }
@@ -40,12 +40,12 @@ namespace Netsoft.OpenDataStructures.Chapter01
             }
             return lines;
         }
-       
+
         public static void Answer03(StreamReader stdin, StreamWriter stdout)
         {
             var lines = stdin.ReadLines(41);
 
-            if(stdin.EndOfStream)
+            if (stdin.EndOfStream)
             {
                 return;
             }
@@ -53,7 +53,7 @@ namespace Netsoft.OpenDataStructures.Chapter01
             do
             {
                 string line = stdin.ReadLine();
-                
+
                 if (string.IsNullOrEmpty(line))
                 {
                     stdout.WriteLine(lines[0]);
@@ -63,6 +63,20 @@ namespace Netsoft.OpenDataStructures.Chapter01
                 lines.Add(line);
 
             } while (!stdin.EndOfStream);
+        }
+
+        public static void Answer04(StreamReader stdin, StreamWriter stdout)
+        {
+            var uniqueLines = new HashSet<string>();
+            while (!stdin.EndOfStream)
+            {
+                string line = stdin.ReadLine();
+                if (!uniqueLines.Contains(line))
+                {
+                    uniqueLines.Add(line);
+                    stdout.WriteLine(line);
+                }
+            }
         }
     }
 }
