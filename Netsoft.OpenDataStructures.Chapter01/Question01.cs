b05170206f3a0d277d6e592d9cd172f38e718388
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-14 16:41:25 +0900

    Q1.1.1

---
+++ Netsoft.OpenDataStructures.Chapter01/Question01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Netsoft.OpenDataStructures.Chapter01
+{
+    public class Question01
+    {
+        public static void Answer01(StreamReader stdin, StreamWriter stdout)
+        {
+            var lines = new Stack<string>();
+            while(!stdin.EndOfStream)
+            {
+                lines.Push(stdin.ReadLine());
+            }
+            while(lines.Count != 0)
+            {
+                stdout.WriteLine(lines.Pop());
+            }
+        }
+    }
+}
